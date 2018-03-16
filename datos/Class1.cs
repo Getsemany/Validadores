e6738f14514ff88bfb781dfc6a181fe9d0f3a390
Getsemany <s16120295@alumnos.itsur.edu.mx>
2018-03-15 22:42:44 -0600

    Subiendo

---
+++ datos/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datos
+{
+    public class Validador
+    {
+        public bool esEntero(string a)
+        {
+            bool res = false;
+            int numero;
+            try
+            {
+                numero = int.Parse(a);
+                if ((numero < 0) || (numero > 100))
+                {
+                    res = true;
+                }
+            }
+            catch { }
+            return res;
+        }
+        public bool esDecimal(string a)
+        {
+            bool res=false;
+            double nu;
+            int n;
+            try
+            {
+                nu = double.Parse(a);
+                try
+                {
+                    n = int.Parse(a);
+                }
+                catch
+                {
+                    res = true;
+                }
+                
+            }
+            catch { }         
+            return res;
+        }
+        public bool esEdad(String a)
+        {
+            bool res = false;
+            int nu;
+            try
+            {
+                nu = int.Parse(a);
+                if ((nu > 0) && (nu < 100))
+                {
+                    res = true;
+                }
+            }
+            catch { }
+            return res;
+        }
+    }
+}
---
+++ vista/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using datos;
+
+namespace vista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Validador da = new Validador();
+            string cadena = "";
+            if (da.esEntero(txtEntero.Text))
+            {
+                cadena += "Entero : " + txtEntero.Text + "\n";
+            }
+            if (da.esDecimal(txtDecimal.Text))
+            {
+                cadena += "Decimal : " + txtDecimal.Text+"\n";
+            }
+            if (da.esEdad(txtEdad.Text))
+            {
+                cadena += "Edad : " + txtEdad.Text + "\n";
+            }
+            if (cadena.Equals(""))
+            {
+                MessageBox.Show("Todas rechazadas");
+            }
+            else
+            {
+                MessageBox.Show("Aceptadas \n" + cadena);
+            }
+        }
+    }
+}
